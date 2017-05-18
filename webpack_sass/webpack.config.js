0946de3011d3322d6f90ab9cf8fef6a6edf5010c
Lars Willemsens <lars@willemsens.org>
2017-05-18 13:17:24 +0200

    Adding Webpack Sass demo.

---
+++ webpack_sass/webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist'
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+            { test: /\.scss$/, loader: "style-loader!css-loader!sass-loader" },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
+            { test: /\.(woff|woff2)$/, loader:"url-loader?prefix=font/&limit=5000" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" }
+        ]
+    },
+    devServer: {
+        contentBase: __dirname + "/dist",
+        watchContentBase: true,
+        compress: true,
+        port: 9000
+    }
+};
